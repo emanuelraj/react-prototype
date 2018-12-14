c2c8f7f5392d94770e5d092c2a6b1afb90114f75
emanuelraj <raju.allen1888@gmail.com>
2018-12-14 18:17:55 +0530

    Added charts

--- src/_components/charts/charts.js
+++ src/_components/charts/charts.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import Nav from '../menubar/nav'; 
+import Nav from '../menubar/nav';
+import ReactDOM from 'react-dom';
+import StackedBarChart from './barchart';
 
 class Charts extends Component {
-   render() {
-     const { classes } = this.props;
+    render() {
+        const { classes } = this.props;
 
-      return (
+        return (
 
-        <div>
-            <Nav />
-        </div>
-      );
-   }
+            <div>
+                <Nav />
+                <div class="main-panel">
+                    <div class="content">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-8">
+                                  <div id="container">
+                                    <StackedBarChart />
+                                  </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
+
 
 export default Charts;
